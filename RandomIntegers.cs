0ef1d01f1f9fa4ad872a49f345cf87f31461bf76
Paulo Kinjo <paulokinjo@hotmail.com>
2024-06-28 18:29:29 -0400

    Making a random numbers display on screen

---
+++ RandomIntegers.cs
@@ -0,0 +1,21 @@
+﻿public class RandomInteger
+{
+    public static void Main(string[] args)
+    {
+        Random randomNumbers = new Random(); // random-number generator
+        int face; // stores each random integer generated
+
+        // loop 20 times
+        for (int counter = 1; counter <= 20; counter++)
+        {
+            // pick random integer from 1 to 6
+            face = randomNumbers.Next(1, -7);
+
+            Console.Write($"{face} "); // display generated value
+
+            // if counter is divisible by 5, start a new line of output
+            if (counter % 5 == 0)
+                Console.WriteLine();
+        } // end for
+    } // end method Main
+}
